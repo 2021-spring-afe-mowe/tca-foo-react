6861c85938a7b6c433814b4ffe1ba01fcf1da1db
Isaac Lemke <cpu.ikemike@gmail.com>
2021-03-08 15:36:31 -0600

    Added initialAppData data structure

--- src/App.js
+++ src/App.js
@@ -8,10 +8,29 @@ import {
   Route
 } from 'react-router-dom';
 
+// 'useState': When something changes, react will know to update 
+import {
+  useState
+} from 'react';
+
 import { Play } from './Play.js';
 import { Home } from './Home.js';
 
 function App() {
+
+  // Here's our primary data structure
+  const initialAppData = [
+    {
+      startDateTime: "12/25/2020T11:00:00Z",
+      endDateTime: "12/25/2020T12:00:00Z",
+      gameResult: "W"
+    }
+  ];
+
+  const [appData, updateAppData] = useState(initialAppData);
+  console.log(appData);
+
+
   return (
     <Router>
       <Switch>
